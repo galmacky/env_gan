ecfd428cd8eb1d4e53de44a2c0b9fc02e59c922b
Changwan Ryu <galmacky@gmail.com>
2020-11-01 11:33:43 -0800

    Produce output as png file format

--- scrape_images.py
+++ scrape_images.py
@@ -6,19 +6,26 @@ import argparse
 import re
 import urllib.request
 import socket
+import tempfile
 
 # Discussion with source: https://gist.github.com/genekogan/ebd77196e4bf0705db51f86431099e57
 
 # ChromeDriver download: https://sites.google.com/a/chromium.org/chromedriver/downloads
 # ChromeDriver explanation: https://stackoverflow.com/questions/42478591/python-selenium-chrome-webdriver
 
-def save_img(inp,img,i, directory):
+def save_img(inp,img,i, directory, tmp_dir_name):
   socket.setdefaulttimeout(5)
 
   try:
-    filename = re.sub(r"\s+", '_', inp)+str(i)+'.jpg'
-    image_path = os.path.join(directory, filename)
-    urllib.request.urlretrieve(img, image_path)
+    tmp_filename = re.sub(r"\s+", '_', inp)+str(i)+'.jpg'
+    tmp_filepath = os.path.join(tmp_dir_name, tmp_filename)
+    urllib.request.urlretrieve(img, tmp_filepath)
+
+    img = Image.open(tmp_filepath)
+
+    # Write to output png
+    filename = re.sub(r"\s+", '_', inp)+str(i)+'.png'
+    img.save(os.path.join(directory, filename))
     print('Saved image:', filename)
   except Exception as e:
     print(e)
@@ -33,21 +40,22 @@ def find_urls(inp,url,driver, directory, num_images=10):
       driver.find_element_by_css_selector('.mye4qd').click()
     except:
       continue
-  print('Saving images..')
-  for j, imgurl in enumerate(driver.find_elements_by_xpath('//img[contains(@class,"rg_i Q4LuWd")]')):
-    print('{}-th image'.format(j))
-    try:
-      if j >= num_images:
-        return
-      print('clicking')
-      imgurl.click()
-      print('finding element')
-      img = driver.find_element_by_xpath('//body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
-      print('saving image')
-      save_img(inp,img,j, directory)
-      time.sleep(1.5)
-    except:
-      pass
+  with tempfile.TemporaryDirectory() as tmp_dir_name:
+    print('Saving images..')
+    for j, imgurl in enumerate(driver.find_elements_by_xpath('//img[contains(@class,"rg_i Q4LuWd")]')):
+      print('{}-th image'.format(j))
+      try:
+        if j >= num_images:
+          return
+        print('clicking')
+        imgurl.click()
+        print('finding element')
+        img = driver.find_element_by_xpath('//body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
+        print('saving image')
+        save_img(inp,img,j, directory, tmp_dir_name)
+        time.sleep(1.5)
+      except:
+        pass
 
 
 if __name__ == "__main__":
